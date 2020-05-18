88700f4c4cb5d5d65f2e57e187b8446700a1c234
Dheyson Alves <dheyson.lucas@hotmail.com>
2020-05-18 15:49:14 -0300

    feat: Adding input basic form

--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
 const emailPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-(function errorCheck() {
-	const form = document.getElementById('formk');
-	const input = document.getElementById('email-input');
-	const uierror = document.querySelector('.ui-error');
+const form = document.getElementById('formk');
+const input = document.getElementById('email-input');
 
-	form.addEventListener('submit', function (e) {
+function validate() {
+	let text = '';
+	if (!emailPattern.test(input.value)) {
+		text = 'Please provide a valid email';
+		input.className = 'ui-error';
+	} else {
+		text = '';
+	}
+	document.getElementById('test').textContent = text;
+}
+
+function submitForm() {
+	const submitButton = document.getElementById("js-btn-submit");
+	submitButton.addEventListener('click', function (e) {
 		e.preventDefault();
-		if (emailPattern != input.value) {
-			const p = document.createElement('p');
-			p.textContent = 'Please provide a valid email'
-			input.style.borderColor = "hsl(0, 93%, 68%)";
-			input.style.borderWidth = "2px";
-			uierror.appendChild(p);
-			form.appendChild(uierror);
-		}
+		validate();
 	});
+}
 
-})();
+function init() {
+	submitForm();
+}
+
+document.addEventListener('DOMContentLoaded', init);
